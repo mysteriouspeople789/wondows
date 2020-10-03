5d39ae50f07f364165ee31fdc790605f5ad7b2ef
mysteriouspeople789 <68906397+mysteriouspeople789@users.noreply.github.com>
2020-10-04 00:09:57 +0800

    dark mode toggling

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+sessionStorage.theme = "dark"
+
 function showDropdown() {
     document.getElementById("dropdown-content").style.display = "block"
 }
 
 function hideDropdown() {
     document.getElementById("dropdown-content").style.display = "none"
+}
+
+function changeTheme() {
+    if(sessionStorage.theme === "dark") {
+        sessionStorage.theme = "light"
+        document.getElementById("body").className = "light"
+    }
+    else {
+        sessionStorage.theme = "dark"
+        document.getElementById("body").className = "dark"
+    }
 }
