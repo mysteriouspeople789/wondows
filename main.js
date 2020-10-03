f6bea774d786e8f2d0524e901304e7a369908974
mysteriouspeople789 <68906397+mysteriouspeople789@users.noreply.github.com>
2020-10-03 20:40:01 +0800

    Fixed "more" clicking bug

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function showDropdown() {
     document.getElementById("dropdown-content").style.display = "block"
 }
+
+function hideDropdown() {
+    document.getElementById("dropdown-content").style.display = "none"
+}
